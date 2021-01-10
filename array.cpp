272413dabc21610f7182cc565e3b7e7fdec36be4
Rohan Kumar <63925505+rohanArbi@users.noreply.github.com>
2021-01-10 23:26:09 +0530

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[100];
+	int i,n;
+	cout<<"How many numbers are in the array? \n";
+	cin>>n;
+	cout<<"Enter the elements \n";
+	for(i=0;i<=n-1;++i)
+	cin>>a[i];
+	cout<<"Contents of the array \n";
+	for(i=0;i<=n-1;++i)
+	cout<<a[i]<<'\t';
+	return 0;
+}
